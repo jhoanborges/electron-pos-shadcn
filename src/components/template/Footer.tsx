071f68ac94f9d448fc6627646882ca3e1da4c5e9
Jhoan Borges <radioactiveboyx@gmail.com>
2025-10-29 06:08:00 -0600

    Initial commit

---
+++ src/components/template/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="font-tomorrow text-muted-foreground inline-flex justify-between text-[0.7rem] uppercase">
+      <p>Made by LuanRoger - Based in Brazil 🇧🇷</p>
+      <p>Powered by Electron</p>
+    </footer>
+  );
+}
---
+++ src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import BaseLayout from "@/layouts/BaseLayout";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+/* import { TanStackRouterDevtools } from '@tanstack/react-router-devtools' */
+
+function Root() {
+  return (
+    <BaseLayout>
+      <Outlet />
+      {/* Uncomment the following line to enable the router devtools */}
+      {/* <TanStackRouterDevtools /> */}
+    </BaseLayout>
+  );
+}
+
+export const Route = createRootRoute({
+  component: Root,
+});
